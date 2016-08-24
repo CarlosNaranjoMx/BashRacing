57aff70656cf9e85ccb634954c9b6ed73e0ce0ee
carlosmx <carlosnaranjo@ciencias.unam.mx>
2016-08-24 05:17:53 -0500

    Tareas de uri

---
+++ uri/arrayhash.py
@@ -0,0 +1,33 @@
+'''
+import sys
+
+while(True):
+	line = sys.stdin.readline().split()
+	if line == []:
+		break
+'''
+arrayhash = []
+#total = 0
+
+d = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":15,"Q":16,"R":17,"S":18,"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25}
+
+n = int(input())
+
+def arrhash():
+    global n
+    for i in range(n):
+        l = int(input())
+        total = 0
+        for j in range(l):
+            m = input()
+            for o in range(len(m)):
+                arrayhash.append(m[o])
+        for k in range(len(arrayhash)):
+            total += int(d.get(arrayhash[k])) + j + k 
+        print(total)
+
+
+arrhash()
+
+
+
